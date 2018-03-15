3781bf06fa529bae6f2c5394d8d88b6122507986
eyalmnm <eyal@em-projects.com>
2018-03-15 12:23:17 +0200

    add clean the connections dictionary by value

--- RestServerB/Managers/ConnectionsManager.cs
+++ RestServerB/Managers/ConnectionsManager.cs
@@ -10,8 +10,17 @@ namespace RestServerB.Managers
         private static Dictionary<string, string> connections = new Dictionary<string, string>();
         //private static string uudi;
 
+        // Remove items by Value (userPassword)
+        // https://stackoverflow.com/questions/1636885/remove-item-in-dictionary-based-on-value
         public static string addConnection(string userPassword)
         {
+            if(true == connections.ContainsValue(userPassword))
+            {
+                foreach (var item in connections.Where(kvp => kvp.Value == userPassword).ToList())
+                {
+                    connections.Remove(item.Key);
+                }
+            }
             string uuid = "";
             do
             {
