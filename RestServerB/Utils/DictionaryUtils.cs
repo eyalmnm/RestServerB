9987f834d6bd397a54d2f25359592ef8ea8cad96
eyalmnm <eyal@em-projects.com>
2018-03-15 13:53:14 +0200

    Add Dictionary utils

---
+++ RestServerB/Utils/DictionaryUtils.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace RestServerB.Utils
+{
+    public class DictionaryUtils
+    {
+        public static void ReadDictionaryData(Dictionary<String, String> data)
+        {
+            foreach(KeyValuePair<String, String> item in data)
+            {
+                Console.WriteLine($"key: {item.Key} value: {item.Value}");
+            }
+        }
+    }
+}
